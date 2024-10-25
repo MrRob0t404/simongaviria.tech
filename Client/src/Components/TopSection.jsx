9970299d300bc8fde5f9f6f1dd3695e7438581ee
Simon Gaviria <simongaviria1@gmail.com>
2024-10-25 14:15:28 -0400

    add css file for top section component

--- Client/src/Components/TopSection.jsx
+++ Client/src/Components/TopSection.jsx
@@ -3,32 +3,28 @@ import ReactIcon from "../assets/icons/React Icon.png";
 
 // Styles
 import "../App.css";
-import "./styles/middle-section.css";
+import "./styles/top-section.css";
 
 const TopSection = () => {
   return (
-    <div id="content-wrapper">
-      <div id="main-content">
-        <div id="top-section" className="section">
-          <div id="info-about-me-wrapper">
-            <div id="info" className="cards">
-              <div id="icon-container">
-                <img src={ReactIcon} alt="React Icon" />
-              </div>
-              <div id="info-text">
-                <h1>
-                  Software Engineer Redefining
-                  <span>User Interfaces</span> and <span>Apps</span> with AI
-                  Driven Design
-                </h1>
-              </div>
-            </div>
+    <div id="top-section" className="section">
+      <div id="info-about-me-wrapper">
+        <div id="info" className="cards">
+          <div id="icon-container">
+            <img src={ReactIcon} alt="React Icon" />
           </div>
-          <div id="self-img">
-            <div id="img-container">
-              <img src={selfImg} alt="Self img" className="animate" />
-            </div>
+          <div id="info-text">
+            <h1>
+              Software Engineer Redefining
+              <span>User Interfaces</span> and <span>Apps</span> with AI Driven
+              Design
+            </h1>
           </div>
+        </div>
+      </div>
+      <div id="self-img">
+        <div id="img-container">
+          <img src={selfImg} alt="Self img" className="animate" />
         </div>
       </div>
     </div>
