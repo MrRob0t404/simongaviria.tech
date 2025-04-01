14cbdb81eb9b8b97c168906619db3da098ec0ca3
Simon Gaviria <simongaviria1@gmail.com>
2025-03-31 20:14:10 -0400

    add almost finished hero bar

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,7 +1,11 @@
-import { Spotlight } from "@/components/ui/spotlight";
+"use client";
 import { cn } from "@/lib/utils";
-
 import React from "react";
+
+import MagicButton from "@/components/ui/MagicButton";
+import { TextGenerateEffect } from "@/components/ui/TextGenerate";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -27,9 +31,36 @@ const Hero = () => {
           />
           {/* Radial gradient for the container to give a faded look */}
           <div className="h-screen w-full pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-          <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
+          {/* <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
             Backgrounds
-          </p>
+            </p> */}
+        </div>
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 ">
+              Dynamic Web Magic with Next.js
+            </h2>
+            <TextGenerateEffect
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              duration={3}
+              filter={false}
+              words={"Transforming Concepts into Seamless User Experiences"}
+            />
+
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+              Hi, I&apos;m Simon, a Full Stack Software Engineer based in New
+              York City
+            </p>
+
+            <a href="#about">
+              <MagicButton
+                title={"Show my work"}
+                icon={<FaLocationArrow />}
+                position="right"
+                handleClick={() => console.log("poof")}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
